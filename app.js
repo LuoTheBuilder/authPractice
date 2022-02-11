38fa4c5c7324971a5f66ba29309a08c308ab3023
LuoTheBuilder <rawbailey@outlook.com>
2022-02-10 20:45:53 -0500

    Authentication Level 2

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 require("dotenv").config();
@@ -12,10 +13,13 @@ mongoose.connect(process.env.Mongo + "userDB", {
   useUnifiedTopology: true,
 });
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-};
+});
+
+const secret = "ThisIsOurLittleSecret";
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
